956c0238ba849b86ac7cadbad7a6d66d40a109c9
Darius Robinson <drobin86@emich.edu>
2026-02-16 16:52:57 -0500

    Im hungry

--- Driver.java
+++ Driver.java
@@ -5,5 +5,6 @@ public class Driver {
 		System.out.println("Our team is getting started");
 		
 		glad.glad();
+		Hungry.hungry();
 }
 }
---
+++ Hungry.java
@@ -0,0 +1,11 @@
+
+public class Hungry {
+
+	public static void hungry() {
+		System.out.println("We all hungry");
+		
+	
+}
+
+}
+
